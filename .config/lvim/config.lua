3d55ec132f0a4666c09b730dcf3a67fdd1a57b7c
sedlund <sedlund@users.noreply.github.com>
2021-12-21 16:57:05 -0600

    add shellcheck

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -136,21 +136,21 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- }
 
 -- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
 --   { exe = "flake8", filetypes = { "python" } },
---   {
---     exe = "shellcheck",
+  {
+    exe = "shellcheck",
 --     ---@usage arguments to pass to the formatter
 --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     args = { "--severity", "warning" },
---   },
+    args = { "--severity", "warning" },
+  },
 --   {
 --     exe = "codespell",
 --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
 --     filetypes = { "javascript", "python" },
 --   },
--- }
+}
 
 -- Additional Plugins
 lvim.plugins = {
