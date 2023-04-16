3848dcb7d934f5b62d978f52317142d0bf586e7e
Scott Edlund <sedlund@users.noreply.github.com>
2023-04-16 07:45:23 +0700

    no noice

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -144,29 +144,29 @@ lvim.plugins = {
   "ellisonleao/gruvbox.nvim",
   "catppuccin/nvim",
   "mbbill/undotree",
-  {  "folke/noice.nvim",
-      config = function()
-        require("noice").setup({
-          lsp = {
-            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-            override = {
-              ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-              ["vim.lsp.util.stylize_markdown"] = true,
-              ["cmp.entry.get_documentation"] = true,
-            },
-          },
-          -- you can enable a preset for easier configuration
-          presets = {
-            bottom_search = true, -- use a classic bottom cmdline for search
-            command_palette = true, -- position the cmdline and popupmenu together
-            long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = false, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = false, -- add a border to hover docs and signature help
-          },
-        })          -- add any options here
-      end,
-  }
-}
+--   {  "folke/noice.nvim",
+--       config = function()
+--         require("noice").setup({
+--           lsp = {
+--             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--             override = {
+--               ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--               ["vim.lsp.util.stylize_markdown"] = true,
+--               ["cmp.entry.get_documentation"] = true,
+--             },
+--           },
+--           -- you can enable a preset for easier configuration
+--           presets = {
+--             bottom_search = true, -- use a classic bottom cmdline for search
+--             command_palette = true, -- position the cmdline and popupmenu together
+--             long_message_to_split = true, -- long messages will be sent to a split
+--             inc_rename = false, -- enables an input dialog for inc-rename.nvim
+--             lsp_doc_border = false, -- add a border to hover docs and signature help
+--           },
+--         })          -- add any options here
+--       end,
+--   }
+-- }
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 -- vim.api.nvim_create_autocmd("FileType", {
 --   pattern = "zsh",
@@ -175,6 +175,8 @@ lvim.plugins = {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+}
+
 -- indent-blankline.nvim options
 require("indent_blankline").setup {
   space_char_blankline = " ",
