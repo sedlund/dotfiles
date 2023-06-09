f032bc45ea35239a11b440a01e3e6d8c002e2862
Scott Edlund <sedlund@users.noreply.github.com>
2023-06-10 00:53:11 +0800

    update: remove old config from lvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,48 +1,22 @@
---[[
- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
- `lvim` is the global options object
-]]
-
--- vim options
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
 vim.opt.relativenumber = true
-
-vim.opt.guicursor = ""
-vim.opt.scrolloff = 8
 vim.opt.colorcolumn = "80"
--- vim.opt.updatetime = 200
-vim.opt.cmdheight = 1
-vim.opt.foldmethod = 'syntax'
--- vim.opt.list = true
 -- vim.opt.listchars = {eol = '↲', tab = '▸ ', trail = '·', lead = '·'}
 vim.opt.listchars = { eol = '↲', tab = '«-»', trail = '·', lead = '·' }
+
 lvim.lsp.document_highlight = false
 -- lvim.builtin.autopairs.active = false
-lvim.builtin.lualine.sections.lualine_y = { "location" }
 lvim.transparent_window = true
 
 -- general
-lvim.log.level = "info"
+-- lvim.log.level = "info"
 lvim.format_on_save = {
   enabled = true,
   pattern = "*.lua",
   timeout = 1000,
 }
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
-
--- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
-lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-
--- -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 
 -- -- Change theme settings
 -- lvim.colorscheme = "lunar"
@@ -52,145 +26,57 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 lvim.colorscheme = "catppuccin"
 
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
--- lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
--- Automatically install missing parsers when entering buffer
-lvim.builtin.treesitter.auto_install = true
-
--- lvim.builtin.treesitter.ignore_install = { "haskell" }
-
--- -- always installed on startup, useful for parsers without a strict filetype
--- lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex" }
-
--- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
-
--- --- disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "stylua" },
---   {
---     command = "prettier",
---     extra_args = { "--print-width", "100" },
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     command = "shellcheck",
---     args = { "--severity", "warning" },
---   },
--- }
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  --   { command = "flake8", filetypes = { "python" } },
-  {
-    command = "shellcheck",
-    --     ---@usage arguments to pass to the formatter
-    --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--severity", "warning" },
-  }
-}
--- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
--- lvim.plugins = {
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
-
 lvim.plugins = {
-  --     {"folke/tokyonight.nvim"},
-  --     {
-  --       "folke/trouble.nvim",
-  --       cmd = "TroubleToggle",
-  --     },
+  -- { "folke/tokyonight.nvim" },
+  -- {
+  --   "folke/trouble.nvim",
+  --   cmd = "TroubleToggle",
+  -- },
+  -- "lukoshkin/highlight-whitespace",
+  "johnfrankmorgan/whitespace.nvim",
   "google/vim-jsonnet",
-  "itspriddle/vim-shellcheck",
+  -- "itspriddle/vim-shellcheck",
   "lukas-reineke/indent-blankline.nvim",
   "lunarvim/colorschemes",
-  -- "folke/toktokyonightyonight.nvim",
+  -- "folke/tokyonight.nvim",
   "ellisonleao/gruvbox.nvim",
   "catppuccin/nvim",
   "mbbill/undotree",
---   {  "folke/noice.nvim",
---       config = function()
---         require("noice").setup({
---           lsp = {
---             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
---             override = {
---               ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
---               ["vim.lsp.util.stylize_markdown"] = true,
---               ["cmp.entry.get_documentation"] = true,
---             },
---           },
---           -- you can enable a preset for easier configuration
---           presets = {
---             bottom_search = true, -- use a classic bottom cmdline for search
---             command_palette = true, -- position the cmdline and popupmenu together
---             long_message_to_split = true, -- long messages will be sent to a split
---             inc_rename = false, -- enables an input dialog for inc-rename.nvim
---             lsp_doc_border = false, -- add a border to hover docs and signature help
---           },
---         })          -- add any options here
---       end,
---   }
--- }
--- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+  {
+    "nacro90/numb.nvim",
+    event = "BufRead",
+    config = function()
+      require("numb").setup {
+        show_numbers = true,    -- Enable 'number' for the window while peeking
+        show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+      }
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    name = "leap",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+  {
+    "windwp/nvim-spectre",
+    event = "BufRead",
+    config = function()
+      require("spectre").setup()
+    end,
+  },
+  {
+    "wfxr/minimap.vim",
+    build = "cargo install --locked code-minimap",
+    config = function()
+      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    end,
+  },
 }
 
--- indent-blankline.nvim options
-require("indent_blankline").setup {
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
-}
-
--- TODO: replace with module or lua
--- https://vim.fandom.com/wiki/Highlight_unwanted_spaces
-vim.cmd [[
-  autocmd ColorScheme * highlight ExtraWhitespace ctermbg=red guibg=red
-  match ExtraWhitespace /\s\+$/
-  autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
-  autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
-  autocmd InsertLeave * match ExtraWhitespace /\s\+$/
-  autocmd BufWinLeave * call clearmatches()
-]]
+vim.cmd("let g:minimap_width = 10")
+vim.cmd("let g:minimap_auto_start = 1")
+vim.cmd("let g:minimap_auto_start_win_enter = 1")
